12c65c88dcbaf738fb276099715c8702c58e2a33
web1610 <web1610@vip.163.com>
2017-09-27 10:53:15 +0800

    修改了readme并且新建了项目主体文件

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # jquxy
 基于jquery1.11.3的表单插件
+<a href="#n1">使用说明</a>
+本项目测试案例放在
+<a href="http://www.baidu.com">baidu.com</a>
+
+
---
+++ getlist.php
@@ -0,0 +1,20 @@
+<?php
+/**
+*此页面不应被客户端直接请求，需要被其它PHP页面包含
+**/
+header('Content-Type: application/json;charset=UTF-8');
+
+$conn = mysqli_connect('127.0.0.1','root','','shopping',3306);
+$sql = "SET NAMES UTF8";
+mysqli_query($conn, $sql);
+
+$output = [];
+
+$sql = "SELECT * from shoplist";
+$result = mysqli_query($conn,$sql);
+$output['data'] = mysqli_fetch_all($result, MYSQLI_ASSOC);
+
+
+echo json_encode($output);
+
+
